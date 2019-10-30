610daaeeffba4e75d345c4cc271806321a1e5f1b
hektormisplon <hektor.misplon@gmail.com>
2019-10-30 19:23:22 +0100

    folder structure

---
+++ bedrock/web/app/themes/wizardsunite/.stylelintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  extends: 'stylelint-config-recommended',
+  rules: {
+    'no-empty-source': null,
+    'no-descending-specificity': null,
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'extend',
+          'at-root',
+          'debug',
+          'warn',
+          'error',
+          'if',
+          'else',
+          'for',
+          'each',
+          'while',
+          'mixin',
+          'include',
+          'content',
+          'return',
+          'function',
+          'tailwind',
+          'apply',
+          'responsive',
+          'variants',
+          'screen',
+        ],
+      },
+    ],
+  },
+};
---
+++ bedrock/web/app/themes/wizardsunite/resources/views/partials/sidebar.blade.php
@@ -0,0 +1,6 @@
+@php dynamic_sidebar('sidebar-primary') @endphp
+
+<div class="col-12 col-md-3">
+    <h2>Sidebar</h2>
+    <p>Sidebar with NO widget area - Homepage (hardcoded text)</p>
+</div>
